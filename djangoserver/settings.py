b28a02c34e9d3a6e5009166890c2859abcf0ab47
KIM JINSOL <gaussian1137@gmail.com>
2018-11-07 21:16:35 +0900

    Update settings.py

--- djangoserver/settings.py
+++ djangoserver/settings.py
@@ -40,9 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_extensions',
-    'dining',
-    'ep03_APIview',
-    'ep08',
+    'dining',   
 ]
 
 MIDDLEWARE = [
