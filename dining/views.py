7b1682014d24f292ae864a573e95f3a29e324a7d
gaussian37 <gaussian1137@gmail.com>
2018-11-09 03:54:43 +0900

    RestaurantViewSet change for adding pagination of list
    RestaurantViewSet change for adding pagination of list
    added self.queryset re-assignment

--- dining/views.py
+++ dining/views.py
@@ -8,10 +8,11 @@ from .pagination import RestaurantPageNumberPagination
 from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework import generics
 
 
-class RestaurantViewSet(viewsets.ModelViewSet):
-    
+class RestaurantViewSet(viewsets.ModelViewSet, generics.ListAPIView):
+
     # Restaurant DB에서 전체 field를 가져 온다.
     queryset = Restaurant.objects.all()
     
@@ -21,11 +22,13 @@ class RestaurantViewSet(viewsets.ModelViewSet):
     # pagination로 기본값 20 페이지 선정
     pagination_class = RestaurantPageNumberPagination
 
+
     '''
     Restaurant list 조회 재정의
     foodCategory와 station 모두에 Parameter가 전달 되었을 경우, 조건 기반으로 조회한다.
     '''
     def list(self, request, *args, **kwargs):
+
         # foodCategory 파라미터
         foodCategory = request.GET.get("foodCategory")
         # station 파라미터
@@ -33,18 +36,17 @@ class RestaurantViewSet(viewsets.ModelViewSet):
 
         # 파라미터가 올바르게 입력 되었는지 확인
         if foodCategory is not None and station is not None:
-            qs = self.queryset.filter(foodCategory=foodCategory, station=station)
+            self.queryset = self.queryset.filter(foodCategory=foodCategory, station=station)
         else:
-            qs = self.queryset
+            pass
 
-        # Serializer 반영
-        serializer = self.get_serializer(qs, many=True)
-        return Response(serializer.data)
+        return super().list(request, *args, **kwargs)
 
     '''
     Restaurant detail 조회 재정희
     특정 Restaurant를 조회하였을 때, 조회수가 +1 되도록 한다.
     '''
+
     def retrieve(self, request, *args, **kwargs):
         # pk값 가져옴
         pk = self.kwargs['pk']
@@ -55,7 +57,7 @@ class RestaurantViewSet(viewsets.ModelViewSet):
         return super().retrieve(request, *args, **kwargs)
 
 
-    '''세부 API 사용 시 아래 참조'''
+    # '''세부 API 사용 시 아래 참조'''
     # @action(detail=False)
     # def main_search(self, request):
     #     foodCategory = request.GET.get("foodCategory")
