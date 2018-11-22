05bb70a3814315742122865dc63e71ab1deb06a9
gaussian37 <gaussian1137@gmail.com>
2018-11-22 19:41:23 +0900

    update

--- djangoserver/settings.py
+++ djangoserver/settings.py
@@ -149,7 +149,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE':20,
+    'PAGE_SIZE':6,
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
          'rest_framework.authentication.BasicAuthentication',
