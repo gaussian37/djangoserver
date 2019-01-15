8b008ed12789f10228548ae7f8424ea874c20106
gaussian37 <gaussian1137@gmail.com>
2019-01-15 22:34:34 +0900

    added scriptfile for like/reviewSetting

---
+++ likeReviewSetting.py
@@ -0,0 +1,33 @@
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoserver.settings')
+django.setup()
+
+from allauth.account.models import EmailAddress, EmailConfirmation
+from allauth.socialaccount.models import SocialAccount, SocialApp, SocialToken
+from dining.models import Image, Like, Restaurant, Review
+from django.contrib.auth.models import Group, Permission, User
+from django.contrib.admin.models import LogEntry
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.sessions.models import Session
+from django.contrib.sites.models import Site
+from rest_framework.authtoken.models import Token
+# Shell Plus Django Imports
+from django.core.cache import cache
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.models import Avg, Case, Count, F, Max, Min, Prefetch, Q, Sum, When, Exists, OuterRef, Subquery
+from django.utils import timezone
+from django.urls import reverse
+# RestaurantViewSet Import
+from dining.views import RestaurantViewSet
+
+# RestaurantViewSet 객체 생성
+rv = RestaurantViewSet()
+# like, review 테이블을 prefetch_related로 모든 객체 불러옵니다.
+qs = Restaurant.objects.all().prefetch_related("like_set", "review_set")
+
+# likeNum과 reviewNum 갱신합니다.
+for q in qs:    
+    rv.saveLikeReviewNum(q)
