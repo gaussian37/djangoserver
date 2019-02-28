defe6ee8143a6171455da1be955b20d98c9612da
KIM JINSOL <gaussian1137@gmail.com>
2019-02-28 15:23:11 +0900

    Update batchImageClassifier.py

--- batchImageClassifier.py
+++ batchImageClassifier.py
@@ -35,42 +35,40 @@ textSize = 20
 model = load_model('/home/bluemen/djangoserver/resources/MobileNet2Weights.h5')
 baseUrl = "http://bluemen.pythonanywhere.com"
 
+# 미분류된 이미지만 불러옵니다.
+images = Image.objects.filter(category=-1).select_related("restaurant")
 
-while(1):
-    # 미분류된 이미지만 불러옵니다.
-    images = Image.objects.filter(category=-1).select_related("restaurant")
+# 미분류된 이미지를 순회하면서 이미지 분류 처리를 합니다.
+# 만약 식당의 대표이미지가 없다면 첫 등록된 음식 이미지를 식당 대표이미지로 지정합니다.
+for image in images:
+    # 저장된 이미의 경로를 가져옵니다.
+    fname = '/home/bluemen/djangoserver/media/' + image.image.name
+    # opencv로 이미지를 읽어 옵니다.
+    img = cv2.imread(fname)
+    # 만약 img가 불러오지 못한다면 무시합니다.
+    if img is None:
+        continue
+    # 학습할 때 적용된 normalize 처리와 BGR을 RGB 타입으로 바꾸어 줍니다.
+    img = img[:,:,::-1]/255.
+    # 학습된 사이즈에 맞게 이미지를 resize 해줍니다.
+    resizedImg = cv2.resize(img, (224, 224))
+    # 학습된 shape에 맞게 reshape을 해줍니다.
+    target = np.reshape(resizedImg, (1, 224, 224, 3))
+    # 학습된 모델에 입력하여 분류를 합니다.
+    resultVector = model.predict(target)
+    # 결과 벡터에서 가장 큰값의 index를 받아 옵니다.
+    resultIndex = np.argmax(resultVector)
+    # index를 category에 저장합니다.
+    image.category = resultIndex
+    # 작업한 내용을 db에 저장합니다.
+    image.save()
 
-    # 미분류된 이미지를 순회하면서 이미지 분류 처리를 합니다.
-    # 만약 식당의 대표이미지가 없다면 첫 등록된 음식 이미지를 식당 대표이미지로 지정합니다.
-    for image in images:
-        # 저장된 이미의 경로를 가져옵니다.
-        fname = '/home/bluemen/djangoserver/media/' + image.image.name
-        # opencv로 이미지를 읽어 옵니다.
-        img = cv2.imread(fname)
-        # 만약 img가 불러오지 못한다면 무시합니다.
-        if img is None:
-            continue
-        # 학습할 때 적용된 normalize 처리와 BGR을 RGB 타입으로 바꾸어 줍니다.
-        img = img[:,:,::-1]/255.
-        # 학습된 사이즈에 맞게 이미지를 resize 해줍니다.
-        resizedImg = cv2.resize(img, (224, 224))
-        # 학습된 shape에 맞게 reshape을 해줍니다.
-        target = np.reshape(resizedImg, (1, 224, 224, 3))
-        # 학습된 모델에 입력하여 분류를 합니다.
-        resultVector = model.predict(target)
-        # 결과 벡터에서 가장 큰값의 index를 받아 옵니다.
-        resultIndex = np.argmax(resultVector)
-        # index를 category에 저장합니다.
-        image.category = resultIndex
+    # 분류한 이미지가 음식(0)이고 식당의 대표이미지가 없다면 대표이미지로 설정합니다.
+    restaurantInstance = image.restaurant
+    if resultIndex==0 and restaurantInstance.representativeImage == "":
+        restaurantInstance.representativeImage = baseUrl + image.image.url
         # 작업한 내용을 db에 저장합니다.
-        image.save()
-
-        # 분류한 이미지가 음식(0)이고 식당의 대표이미지가 없다면 대표이미지로 설정합니다.
-        restaurantInstance = image.restaurant
-        if resultIndex==0 and restaurantInstance.representativeImage == "":
-            restaurantInstance.representativeImage = baseUrl + image.image.url
-            # 작업한 내용을 db에 저장합니다.
-            restaurantInstance.save()        
+        restaurantInstance.save()        
 
 
 # # 미분류된 이미지를 분류합니다.
